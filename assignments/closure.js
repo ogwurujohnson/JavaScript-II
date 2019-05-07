fe6945cdf969bc2e3adf6c19923f15f120b7cb97
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-07 13:34:40 +0100

    Feat: Add a me function to practice more closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const Johnson = () => {
+  const name = 'Johnson Ogwuru';
 
+  return () => {
+    const address = 'lagos, nigeria';
+    return `my name is ${name} i stay in ${address}`;
+  }
+};
+const me = Johnson();
+console.log(me());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -20,7 +29,22 @@ console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  count = 0;
+  return {
+    increment() {
+      count ++;
+      return count;
+    },
+    decrement() {
+      count --;
+      return count;
+    }
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+console.log(counterFactory().decrement());
+console.log(counterFactory().increment());
+
