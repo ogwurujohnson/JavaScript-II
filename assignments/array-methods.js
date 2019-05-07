61f633a3b9badcde8ac6f06766fe580d32b6b2b7
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-07 13:56:43 +0100

    Feat: Return a list of compsny nsmes in capital letter using .forEach

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -85,6 +85,7 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+    upperCompany.push(runner.company_name.toUpperCase());
 
 // Problem 2
 
